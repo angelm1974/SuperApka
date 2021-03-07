2f2bddaaf97a6193b25fc602db778912683ca7f4
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-03-07 19:11:12 +0100

    zrzut2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MatSliderModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
